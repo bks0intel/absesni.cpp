e610cfbc133680a0dce0a41be6456c912c204a35
firas <firasabt12@gmail.com>
2024-02-16 16:26:27 +0700

    simple input

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
 
-int main(){
-    std::cout << "hello world!!\n";
+int main()
+{
+    int com;
+    do
+    {
+        std::cout << "please select a command bellow\n";
+        std::cout << "4.exit\n";
+        std::cin >> com ;
+        if(com > 4)
+        {
+            system("cls");
+            std::cout << "wrong input";
+            Sleep(250);
+            system("cls");
+        
+
+        }
+
+    }while (com != 4);
+    
+    
 }
